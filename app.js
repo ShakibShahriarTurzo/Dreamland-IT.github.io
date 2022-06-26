a81356df2a079b5f36db83c03e99688751bda506
Shakib Shahriar Turzo <aq0976848@gmail.com>
2022-06-26 14:23:12 +0600

    Dreamland It

---
+++ app.js
@@ -0,0 +1,23 @@
+function bangla(){
+  let click2 = document.getElementById("convert").innerHTML = "একটি ওয়েবসাইট হল এক সর্বজনীনভাবে অ্যাক্সেসযোগ্য, আন্তঃলিঙ্কযুক্ত ওয়েব পৃষ্ঠাগুলির সংগ্রহ যা একটি একক ডোমেন নাম ভাগ করে। ওয়েবসাইটগুলি বিভিন্ন উদ্দেশ্যে পরিবেশন "
+}
+function english(){
+  let click1 = document.getElementById("convert").innerHTML = "A website is a collection of publicly accessible, interlinked Web pages that share a single domain name. Websites can be created and maintained by an individual, group, business, or organization to serve a variety of purposes."
+
+}
+
+function banglaone(){
+  let click3 = document.getElementById("convertt").innerHTML = "একটি ডাটাবেস হল কাঠামোগত তথ্যের একটি সংগঠিত সংগ্রহ, বা ডেটা, সাধারণত একটি কম্পিউটার সিস্টেমে ইলেকট্রনিকভাবে সংরক্ষণ করা হয়"
+}
+function englishone(){
+  let click4 = document.getElementById("convertt").innerHTML = "A database is an organized collection of structured information, or data, typically stored electronically in a computer system"
+
+}
+
+function banglatwo(){
+  let click3 = document.getElementById("converttt").innerHTML = "স্যার টিমোথি জন বার্নার্স-লি OM KBE FRS FREng FRSA DFBCS (জন্ম 8 জুন 1955),[1] টিমবিএল নামেও পরিচিত, একজন ইংরেজি কম্পিউটার বিজ্ঞানী যিনি ওয়ার্ল্ড ওয়াইড ওয়েবের উদ্ভাবক হিসেবে পরিচিত।"
+}
+function englishtwo(){
+  let click4 = document.getElementById("converttt").innerHTML = "Sir Timothy John Berners-Lee OM KBE FRS FREng FRSA DFBCS (born 8 June 1955),[1] also known as TimBL, is an English computer scientist best known as the inventor of the World Wide Web. "
+
+}
